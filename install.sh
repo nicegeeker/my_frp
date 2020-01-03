87a24c81066c70e6043f33382d88398ecb7cc9d9
nicegeeker <xuyiqun.xyq@gmail.com>
2020-01-03 20:57:12 +0800

    mistake modified

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 read -p "Enter the frps version you want to install(example: 0.30.0):" FRP_VERSION
 read -p "Enter the frps platform you want to install(amd64\arm\arm64):" FRP_PLATFORM
-read -p "Server for Client(frps\frpc)" FRP_CORS
+read -p "Server for Client(frps\frpc):" FRP_CORS
 
-FILE="frp_${FRPS_VERSION}_linux_${FRP_PLATFORM}"
+FILE="frp_${FRP_VERSION}_linux_${FRP_PLATFORM}"
 
 if [ ! -f "${FILE}.tar.gz" ]
 then
   echo -e "Downloading!\n"
-  wget https://github.com/fatedier/frp/releases/download/v${FRPS_VERSION}/${FILE}.tar.gz
+  wget https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE}.tar.gz
 fi
 
 tar -xf ${FILE}.tar.gz
@@ -27,17 +27,17 @@ then
   read -p "Enter authentic token" TOKEN
   read -p "Enter dashboard_user:" DASH_USER
   read -p "Enter dashboard_passwd:" DASH_PASSWD
-  sed -i 's/TOKEN/${TOKEN}/g' frps.ini
-  sed -i 's/DASH_USER/${DASH_USER}/g' frps.ini
-  sed -i 's/DASH_PASSWD/${DASH_PASSWD}/g' frps.ini
+  sed -i "s/TOKEN/${TOKEN}/g" frps.ini
+  sed -i "s/DASH_USER/${DASH_USER}/g" frps.ini
+  sed -i "s/DASH_PASSWD/${DASH_PASSWD}/g" frps.ini
 
 else
   touch frpc.ini
   cat frpc_template.ini >> frpc.ini
   read -p "Enter authentic token:" TOKEN
   read -p "Enter server address:" SERVER_ADDR
-  sed -i 's/TOKEN/${TOKEN}/g' frpc.ini
-  sed -i 's/SERVER_ADDR/${SERVER_ADDR}/g' frpc.ini
+  sed -i "s/TOKEN/${TOKEN}/g" frpc.ini
+  sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" frpc.ini
 fi
 
 
